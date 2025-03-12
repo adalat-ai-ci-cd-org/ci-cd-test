61ca2d64aaa093e7c5c425222a4a0e53df719edf
mura108 <mura@adalat.ai>
2025-03-12 21:50:34 +0530

    adding github action token generation changes

--- access_token.sh
+++ access_token.sh
@@ -1,5 +1,5 @@
-INSTALLATION_ID=62382783  # Replace with your actual Installation ID
-JWT_TOKEN="eyJhbGciOiJSUzI1NiJ9.eyJpYXQiOjE3NDE3NzEwMzgsImV4cCI6MTc0MTc3MTY5OCwiaXNzIjoxMTcyNDczfQ.yRYT-tY634yET1lj4mN9uPy6mYYwkevVqtYGG09nB8xfwoaksd_pmfCS_CgICj0BqCjqDFKKFy6ehoFUgz6HoezSxuxG5Hkn3yHYhN7lxN4T-QaftL3Q08OxvQJ4m8ucl-HXSXSYij6Ru1ioRyvbf7wWCo_P7jGIYcEKbXwTwLOHbIr5oH0KjElqnrTHt_BLHHnThqpGxFzYVIpZIn3Bq43pRenjyLbBi596r4YV1dlFs_eoHd44REO0UPn3LOTgwIViBSDYcddM5cMayVFQhntxdJXEn9IZc6nqOyUyP4Rhl1bruK5r6spmTeO85oTSiMwqC9mnumLWd8PbJV2XcQ"  # Replace this with the token generated in Step 3
+INSTALLATION_ID=62502394  # Replace with your actual Installation ID
+JWT_TOKEN="eyJhbGciOiJSUzI1NiJ9.eyJpYXQiOjE3NDE3NzQ3NDgsImV4cCI6MTc0MTc3NTQwOCwiaXNzIjoxMTcyNDczfQ.jPQXfGsEql2wl3j6i7zxiFtBxyqi4rLVLAXA3B3XwQB3jhz2H4WmMhdrQ87uw83LzjNchy_08DBF-i2ia2RCgDrUisgN6cQKwCwGZxuvjKNntAWStbuX7KCpkUqc0LCEugdMQRgoEEo3D4LUnGnoDVHdtg2k9DdHIxGKIvpDBjLIZa7GEnh-iVmaDNSNWxZ4Atc423Ode5zz9bChnS_dtUMH3iqfKH42LUwXI4kttof7P-QfDPl60Nrsh4v6qKPuOVHT7aoFp8nsq3W6T8p_--NcpZeUs6mCjLp1Oh4h1qGNsZQJtfB-Cvo0faaJspPngWzz90OOje7w6QoqynMyOQ"  # Replace this with the token generated in Step 3
 
-curl -X POST -H "Authorization: Bearer $JWT_TOKEN" -H "Accept: application/vnd.github+json" \
+curl -s -X POST -H "Authorization: Bearer $JWT_TOKEN" -H "Accept: application/vnd.github+json" \
   "https://api.github.com/app/installations/${INSTALLATION_ID}/access_tokens"
